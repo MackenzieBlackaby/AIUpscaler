481358eaeb968e442f0030509bec1d660d109829
Mackenzie <54173553+ToxikDnb@users.noreply.github.com>
2026-02-04 13:46:10 +0000

    Created a basic model of a SuperResolution convolutional neural network

---
+++ models/cnn.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+
+"""
+
+This file is a model of a CNN used to upscale images using AI
+We build this neural network out of a single repeating convolutional block that calculates the residual
+This allows us to customise the power of the network through a single integer on the fly.
+
+"""
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channelCount: int):
+        super().__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(channelCount, channelCount, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channelCount, channelCount, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+
+class UpsampleBlock(nn.Module):
+    def __init__(self, features: int, scale: int):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(features, features * (scale**2), kernel_size=3, padding=1),
+            nn.PixelShuffle(scale),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class SupResNet(nn.Module):
+    def __init__(
+        self, scale: int = 2, channels: int = 3, features: int = 64, blockCount: int = 8
+    ):
+        super().__init__()
+        assert scale in (2, 3, 4)
+        self.scale = scale
+        self.head = nn.Sequential(
+            nn.Conv2d(channels, features, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.body = nn.Sequential(*[ResidualBlock(features) for _ in range(blockCount)])
+        if scale in (2, 3):
+            self.upsampleSection = nn.Sequential(UpsampleBlock(features, scale))
+        else:
+            self.upsampleSection = nn.Sequential(
+                UpsampleBlock(features, 2), UpsampleBlock(features, 2)
+            )
+        self.tail = nn.Conv2d(features, channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        base = nn.functional.interpolate(
+            x, scale_factor=self.scale, mode="bicubic", align_corners=False
+        )
+        feature = self.head(x)
+        feature = self.body(feature)
+        feature = self.upsampleSection(feature)
+        residual = self.tail(feature)
+        output = base + residual
+        return output.clamp(0.0, 1.0)
