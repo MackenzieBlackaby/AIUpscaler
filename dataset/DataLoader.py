8b0d99e642a0a427f4ce8386fb3737889353fd22
Mackenzie <54173553+ToxikDnb@users.noreply.github.com>
2026-02-05 15:57:51 +0000

    Created a dataloader class for easy learning

---
+++ dataset/DataLoader.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from PIL import Image
+from random import randint
+from torch.utils.data import Dataset
+import torchvision.transforms.functional as TF
+from torch import Tensor
+from dataclasses import dataclass
+
+@dataclass
+class ImagePair:
+    lowRes: Tensor
+    highRes: Tensor
+
+class ImageSet(Dataset):
+    def __init__(self, rootDir: str, scale: int = 2, hrCrop: int = 192):
+        self.suffixes = {".jpg", ".png", ".jpeg", ".webp"}
+        self.paths = sorted(
+            [p for p in Path(rootDir).rglob("*") if p.suffix.lower() in self.suffixes]
+        )
+        self.scale = scale
+        self.hrCrop = hrCrop
+
+    def __len__(self):
+        return len(self.paths)
+
+    # Returns low res and high res tensor pair
+    def __getitem__(self, idx):
+        # Get img
+        img = Image.open(self.paths[idx]).convert("RGB")
+
+        # Size up smaller images
+        w, h = img.size
+        if w < self.hrCrop or h < self.hrCrop:
+            img = img.resize((max(w, self.hrCrop), max(h, self.hrCrop)), Image.BICUBIC)
+
+        # Random Crop
+        x = randint(0, w - self.hrCrop)
+        y = randint(0, h - self.hrCrop)
+        highRes = img.crop((x, y, x + self.hrCrop, y + self.hrCrop))
+
+        lowResSize = self.hrCrop // self.scale
+        lowRes = highRes.resize((lowResSize, lowResSize), Image.BICUBIC)
+
+        return ImagePair(TF.to_tensor(lowRes), TF.to_tensor(highRes))
