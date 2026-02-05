b78d9ec819d55d1e039f25e79a340196a3d8220c
Mackenzie <54173553+ToxikDnb@users.noreply.github.com>
2026-02-05 18:51:27 +0000

    added runnable section to data downloader

--- dataset/DataDownloader.py
+++ dataset/DataDownloader.py
@@ -1,6 +1,12 @@
 from kagglehub import dataset_download
 
+
 # Downloads the data on the fly. Default flickr2k dataset
 def DownloadData(name: str = "daehoyang/flickr2k"):
     path = dataset_download("daehoyang/flickr2k")
     return path
+
+
+if __name__ == "__main__":
+    DownloadData()
+    pass
