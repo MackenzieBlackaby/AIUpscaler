5fdbdd876c7ea840f1c7d6ece497f7b683c929cc
Mackenzie <54173553+ToxikDnb@users.noreply.github.com>
2026-02-08 20:48:29 +0000

    Added a very optional downscaling style parameter

--- api/utility.py
+++ api/utility.py
@@ -7,6 +7,7 @@ def simulateLowRes(
     highResImage: Image.Image,
     scale: int,
     upscalingStyle: Optional[Resampling] = Resampling.BICUBIC,
+    downscalingStyle: Optional[Resampling] = Resampling.BICUBIC,
 ) -> tuple[Image.Image, Image.Image]:
     """
     Simulates a low resolution image by downscaling a high resolution image, then upscaling with a specified upscaling style
@@ -16,14 +17,16 @@ def simulateLowRes(
     :param scale: The factor to downscale the image by
     :type scale: int
     :param upscalingStyle: The PIL Image function to upscale the image with. Default is BICUBIC
-    :type upscalingStyle: function
+    :type upscalingStyle: Resampling
+    :param downscalingStyle: (BETTER TO LEAVE DEFAULT) The PIL Image function to downscale the image with. Default is Bicubic
+    :type downscalingStyle: Resampling
     :return: A tuple of the low resolution image and the upscaled low resolution image
     :rtype: tuple[Image.Image, Image.Image]
     """
     width, height = highResImage.size
     lowResWidth, lowResHeight = max(1, width // scale), max(1, height // scale)
     lowResImage = highResImage.resize(
-        (lowResWidth, lowResHeight), resample=Resampling.BICUBIC
+        (lowResWidth, lowResHeight), resample=downscalingStyle
     )
     lowResUpscaledImage = lowResImage.resize((width, height), resample=upscalingStyle)
     return (lowResImage, lowResUpscaledImage)
