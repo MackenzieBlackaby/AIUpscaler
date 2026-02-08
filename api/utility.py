098355825960e2d8b9bba9008c428b465d5a74ab
Mackenzie <54173553+ToxikDnb@users.noreply.github.com>
2026-02-08 20:42:10 +0000

    Added a utility API to give extra tools useful for development

---
+++ api/utility.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from PIL.Image import Resampling
+from typing import Optional
+
+
+def simulateLowRes(
+    highResImage: Image.Image,
+    scale: int,
+    upscalingStyle: Optional[Resampling] = Resampling.BICUBIC,
+) -> tuple[Image.Image, Image.Image]:
+    """
+    Simulates a low resolution image by downscaling a high resolution image, then upscaling with a specified upscaling style
+
+    :param highResImage: The original high resolution image
+    :type highResImage: Image.Image
+    :param scale: The factor to downscale the image by
+    :type scale: int
+    :param upscalingStyle: The PIL Image function to upscale the image with. Default is BICUBIC
+    :type upscalingStyle: function
+    :return: A tuple of the low resolution image and the upscaled low resolution image
+    :rtype: tuple[Image.Image, Image.Image]
+    """
+    width, height = highResImage.size
+    lowResWidth, lowResHeight = max(1, width // scale), max(1, height // scale)
+    lowResImage = highResImage.resize(
+        (lowResWidth, lowResHeight), resample=Resampling.BICUBIC
+    )
+    lowResUpscaledImage = lowResImage.resize((width, height), resample=upscalingStyle)
+    return (lowResImage, lowResUpscaledImage)
